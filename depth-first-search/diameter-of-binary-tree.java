9c0328b3597318f74f988875c3bb9fde2b9e373c
Mithun <155316976+Mithun20222@users.noreply.github.com>
2026-04-09 18:35:17 +0530

    LC-diameter-of-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/diameter-of-binary-tree.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int h = 0;
+        return height(root.left)+height(root.right);
+    }
+    public int height(TreeNode root){
+        if(root == null) return 0;
+        int hr = height(root.right);
+        int hl = height(root.left);
+        return Math.max(hr,hl)+1;
+    }
+}
